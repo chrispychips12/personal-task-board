b6157e828fb79b82d39afc987f0bbec1542026aa
Christopher <94519893+chrispychips12@users.noreply.github.com>
2024-07-06 11:52:14 +1000

    Created new function to handle new tasks in JS

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -3,11 +3,12 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {const id = nextId; 
-    nextId++; 
+function generateTaskId() {
+    const id = nextId;
+    nextId++;
     localStorage.setItem("nextId", JSON.stringify(nextId)); // Save the updated nextId to localStorage
-    return id; 
-  }
+    return id;
+}
 
 
 // Todo: create a function to create a task card
@@ -25,8 +26,8 @@ function createTaskCard(task) {
     card.append(cardBody);
 
     return card;
-  }
-  
+}
+
 
 // Todo: create a function to render the task list and make cards draggable
 // Function to render the task list and make cards draggable
@@ -35,53 +36,79 @@ function renderTaskList() {
     $('#todo-cards').empty();
     $('#in-progress-cards').empty();
     $('#done-cards').empty();
-  
+
     // Iteratation over taskList + create new task cards
     taskList.forEach(task => {
-      const taskCard = createTaskCard(task);
-      if (task.status === 'to-do') {
-        $('#todo-cards').append(taskCard);
-      } else if (task.status === 'in-progress') {
-        $('#in-progress-cards').append(taskCard);
-      } else if (task.status === 'done') {
-        $('#done-cards').append(taskCard);
-      }
+        const taskCard = createTaskCard(task);
+        if (task.status === 'to-do') {
+            $('#todo-cards').append(taskCard);
+        } else if (task.status === 'in-progress') {
+            $('#in-progress-cards').append(taskCard);
+        } else if (task.status === 'done') {
+            $('#done-cards').append(taskCard);
+        }
     });
-  
+
     // Made task cards draggable
     // https://jqueryui.com/draggable/
     // https://api.jqueryui.com/draggable/
     // https://www.tutorialspoint.com/jqueryui/jqueryui_draggable.htm
     $('.card').draggable({
-      revert: "invalid",
-      helper: "clone",
-      start: function (event, ui) {
-        $(this).hide();
-      },
-      stop: function (event, ui) {
-        $(this).show();
-      }
+        revert: "invalid",
+        helper: "clone",
+        start: function (event, ui) {
+            $(this).hide();
+        },
+        stop: function (event, ui) {
+            $(this).show();
+        }
     });
-  
+
     // + also droppable
     // https://jqueryui.com/droppable/
     // https://www.tutorialspoint.com/jqueryui/jqueryui_droppable.htm
     $('.lane').droppable({
-      accept: ".card",
-      drop: function (event, ui) {
-        handleDrop(event, ui);
-      }
+        accept: ".card",
+        drop: function (event, ui) {
+            handleDrop(event, ui);
+        }
     });
-  }
-  
-
-// Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
 }
 
+
+// Todo: create a function to handle adding a new task
+// https://stackoverflow.com/questions/22195065/how-to-send-a-json-object-using-html-form-data
+// https://stackoverflow.com/questions/67088439/what-event-handler-should-i-use-for-modal-form-to-send-an-email
+
+function handleAddTask(event) {
+    event.preventDefault();
+
+    // Collection of task details from the form
+    const title = $('#taskTitle').val();
+    const description = $('#taskDescription').val();
+    const deadline = $('#taskDeadline').val();
+    const id = generateTaskId(); // Generate new task ID
+    const newTask = { // Create new object
+        id: id,
+        title: title,
+        description: description,
+        deadline: deadline,
+        status: 'to-do'
+    };
+
+
+    taskList.push(newTask); // Add task
+    localStorage.setItem('tasks', JSON.stringify(taskList)); // Save new task 
+    renderTaskList(); // Redner new task
+
+    $('#taskForm')[0].reset();
+    $('#formModal').modal('hide');
+}
+$('#taskForm').on('submit', handleAddTask);
+
+
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
